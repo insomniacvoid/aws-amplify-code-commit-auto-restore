bb6fc3ac177fc907098302153d8636ca45b515fb
void <yane.clarke@gmail.com>
2023-05-13 08:08:17 +1000

    variable should be global

--- domain_setup.sh
+++ domain_setup.sh
@@ -2,18 +2,17 @@
 
 # Run domain setup
 domain_setup() {
-  local certificate_arn
 
-  setup_dnssec
+setup_dnssec
 
-  #   Remove any existing domain associations
+  # Remove any existing domain associations
   remove_domain_association
 
-  #   Wait for the branch to become available
+  # Wait for the branch to become available
   echo "Waiting for the branch to become available..."
   sleep 30
 
-  #   Create the domain association with the root domain and 'dev' subdomain
+  # Create the domain association with the root domain and 'dev' subdomain
   echo "Creating new domain association"
 
   check_command aws amplify create-domain-association \
@@ -22,9 +21,9 @@ domain_setup() {
     --sub-domain-settings '[{"prefix": "", "branchName": "master"}, {"prefix": "www", "branchName": "master"}, {"prefix": "dev", "branchName": "dev"}]' \
     --no-cli-pager
 
-  #   Request a new public SSL/TLS certificate
+  # Request a new public SSL/TLS certificate
   certificate_arn=$(request_certificate)
 
-  #   Add and verify the CNAME record for domain ownership
+  # Add and verify the CNAME record for domain ownership
   add_and_verify_cname_record "$certificate_arn"
 }
